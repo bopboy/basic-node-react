39591b2edcdbd73dd65e18880b12ccc0f49fb4bf
bopboy2 <scp0000@gmail.com>
2021-09-16 06:58:10 +0900

    로그아웃 기능 추가 및 로그인 토큰 생성 위치 오류 수정

--- server/index.js
+++ server/index.js
@@ -30,10 +30,10 @@ app.post('/api/users/login', (req, res) => {
         if (!user) return res.json({ loginSuccess: false, message: "그런 이메일이 없습니다" })
         user.comparePassword(req.body.password, (err, isMatch) => {
             if (!isMatch) return res.json({ loginSuccess: false, message: "비번 틀렸습니다" })
-        })
-        user.generateToken((err, user) => {
-            if (err) return res.json(400).send(err)
-            res.cookie('x_auth', user.token).status(200).json({ loginSuccess: true, userId: user._id })
+            user.generateToken((err, user) => {
+                if (err) return res.json(400).send(err)
+                res.cookie('x_auth', user.token).status(200).json({ loginSuccess: true, userId: user._id })
+            })
         })
     })
 })
